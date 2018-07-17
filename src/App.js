57343af403e8426bc723911bff1b89b93f3c8148
Psylosyn <psy.losyn@gmail.com>
2018-07-17 22:27:08 +1200

    added base functionaliy and style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
 import './App.css';
 
+// create a state component
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      // these change in the app
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  // listener that reports search changes (grabs this from SearchBox)
+  onSearchChange = (event) => {
+    // updates state of searchfield to searched term
+    this.setState({ searchfield: event.target.value })
+  }
+
+  // renders view based on state (which in turn is based on search term)
   render() {
+    // outputs the robot name if it is equal to the search term
+    const filteredRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='tc'>
+        <h1 className='f1'>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}/>
+        <CardList robots={filteredRobots}/>
       </div>
     );
   }
